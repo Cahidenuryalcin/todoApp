497977eb8f63d208792dad78bde92d4dad7a6aab
Cahidenuryalcin <cnuryal@gmail.com>
2024-05-21 19:32:43 +0300

    21.05.24

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,7 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/provider/auth_provider.dart';
 import 'package:todo_app/app_constanst.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,18 +11,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  late FirebaseAuth auth;
-
-  @override
-  void initState() {
-    super.initState();
-    auth = FirebaseAuth.instance;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,43 +22,47 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Sabitler.appbarColor,
-        title: const Text("LOGIN", style: TextStyle(fontFamily: "Montserrat", fontSize: 24, fontWeight: FontWeight.bold, color: Sabitler.appbarTextColor)),
+        title: const Text("LOGIN",
+            style: TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Sabitler.appbarTextColor)),
       ),
       body: Container(
         padding: const EdgeInsets.only(left: 29, right: 29, top: 30),
         child: Form(
-          //key: formKey,  
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
-                onSaved: (newValue) {
-                  
-                },
-                //onSaved: (value) => taskTitle = value ?? "",
-                validator: (value) => value!.isEmpty ? "Please enter your email" : null,
+                controller: _emailController,
+                validator: (value) =>
+                    value!.isEmpty ? "Please enter your email" : null,
                 keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(hintText: "Email"),
               ),
               const SizedBox(height: 10),
               TextFormField(
                 controller: _passwordController,
-                onSaved: (newValue) {
-                  
-                },
-                validator: (value) => value!.isEmpty ? "Please enter password" : null,
+                validator: (value) =>
+                    value!.isEmpty ? "Please enter password" : null,
                 keyboardType: TextInputType.visiblePassword,
                 decoration: const InputDecoration(hintText: "Password"),
                 obscureText: true,
               ),
-              
               const SizedBox(height: 40),
               SizedBox(
                 width: double.infinity,
                 height: 65,
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)), backgroundColor: const Color.fromRGBO(147, 149, 211, 1)),
-                  onPressed: (){},
-                  child: const Text("Login", style: TextStyle(color: Colors.white, fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: const Color.fromRGBO(147, 149, 211, 1)),
+                  onPressed: _login,
+                  child: const Text("Login",
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
                 ),
               )
             ],
@@ -77,5 +70,16 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  void _login() async {
+    if (_formKey.currentState!.validate()) {
+      final authProvider = Provider.of<AuthProvider>(context, listen: false);
+      await authProvider.loginUserEmailAndPassword(
+        _emailController.text,
+        _passwordController.text,
+      );
+      // Additional logic after login, like navigating to a different screen
+    }
   }
 }
