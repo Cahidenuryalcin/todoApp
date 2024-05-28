5ee65d742194c2a4fdcb2e8d5856bb3062ac369a
Cahidenuryalcin <cnuryal@gmail.com>
2024-05-28 23:53:41 +0300

    28.05.24

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
 class AuthProvider with ChangeNotifier {
-  final FirebaseAuth _auth = FirebaseAuth.instance; // _auth => firebaseauth özelliğini kullanmak için değişken
-  User? _user; // user değişkeni
-  final FlutterSecureStorage storage = const FlutterSecureStorage(); // oturumun açık kalması içün
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+  final FlutterSecureStorage storage = const FlutterSecureStorage();
 
-  User? get user => _user; //_user'a eriş, ama deiştireme
+  User? get user => _user;
 
   AuthProvider() {
-    _auth.authStateChanges().listen(_onAuthStateChanged); // sınıfın constructor'ı, kullanıcı işlemleri yaptığında durum değişimi için tetiklenir
+    _auth.authStateChanges().listen(_onAuthStateChanged);
   }
 
-  Future<void> _onAuthStateChanged(User? user) async { // kullanıcı oturum durumu değiştiğinde, user varsa user null olmaktan çıkar ve tüm arayüzlere bildirilir
+  Future<void> _onAuthStateChanged(User? user) async {
     _user = user;
-     if (user != null) {
+    if (user != null) {
       await storage.write(key: 'uid', value: user.uid);
     } else {
       await storage.delete(key: 'uid');
@@ -24,44 +23,39 @@ class AuthProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<UserCredential> loginUserEmailAndPassword(
-      String email, String password) async {
-    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: email, password: password);
+  Future<UserCredential> loginUserEmailAndPassword(String email, String password) async {
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
     _user = userCredential.user;
     return userCredential;
   }
 
-  
-  Future<UserCredential> createUserEmailAndPassword(
-      String email, String password, String name) async {
-    UserCredential userCredential = await _auth
-        .createUserWithEmailAndPassword(email: email, password: password);
+  Future<UserCredential> createUserEmailAndPassword(String email, String password, String name) async {
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
     await userCredential.user?.updateDisplayName(name);
-    _user = userCredential.user;
+    await userCredential.user?.reload();  // Reload the user to ensure the display name is updated
+    _user = _auth.currentUser;  // Update the current user instance
 
     if (!_user!.emailVerified) {
       _user?.sendEmailVerification();
     }
+    notifyListeners();  // Notify listeners after user is updated
     return userCredential;
   }
 
-
   Future<void> signOutUser() async {
     await _auth.signOut();
-     _user = null;
+    _user = null;
     await storage.delete(key: 'uid');
     notifyListeners();
     debugPrint("cikis yapildi");
   }
 
-   Future<void> deleteUser() async{
-    if (_auth.currentUser!= null) {
-       await _auth.currentUser!.delete();
-       debugPrint("kullanıcı silindi");
-    }else{
+  Future<void> deleteUser() async {
+    if (_auth.currentUser != null) {
+      await _auth.currentUser!.delete();
+      debugPrint("kullanıcı silindi");
+    } else {
       debugPrint("önce kayit ol ");
     }
-   
   }
 }
--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/app_constanst.dart';
 import 'package:todo_app/main.dart';
-
 import 'package:todo_app/provider/auth_provider.dart' as my_auth;
-
 
 class UserProfile extends StatefulWidget {
   const UserProfile({super.key});
@@ -16,47 +14,78 @@ class UserProfile extends StatefulWidget {
 class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<my_auth.AuthProvider>(context);
+    final userName = authProvider.user?.displayName ?? 'Kullanıcı';
+
     return Scaffold(
-  
       appBar: AppBar(
-          iconTheme: const IconThemeData(
-            color: Colors
-                .yellow, 
-          ),
-          backgroundColor: Sabitler.appbarColor,
-          title: const Text(
-            "USER PROFILE",
-            style: TextStyle(
-              fontFamily: "Montserrat",
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Sabitler.appbarTextColor,
-            ),
+        iconTheme: const IconThemeData(
+          color: Colors.yellow,
+        ),
+        backgroundColor: Sabitler.appbarColor,
+        title: const Text(
+          "USER PROFILE",
+          style: TextStyle(
+            fontFamily: "Montserrat",
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Sabitler.appbarTextColor,
           ),
         ),
-        body: Card(
-        color: Sabitler.backgroundColor,
-        
-          child: Card(
-            
-            child:ElevatedButton(
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey,
+              child: Icon(
+                Icons.person,
+                size: 50,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Merhaba $userName',
+              style: TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Sabitler.appbarColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
               onPressed: () {
                 _logOut();
               },
-              child: Text('Log Out'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Sabitler.floatingActionButtonColor,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              ),
+              child: Text(
+                'Log Out',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Sabitler.tabbarColor,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
+      ),
     );
-    
   }
-  
+
   void _logOut() {
     final authProvider = Provider.of<my_auth.AuthProvider>(context, listen: false);
-    authProvider.signOutUser(); // Oturumu sonlandır
-     Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => InitialScreen()),
-      );
+    authProvider.signOutUser();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) =>  InitialScreen()),
+    );
   }
 }
